8a303d481ccf33e098fa90e14f46a80a7ddd60bc
daniellesalman <salman.danielle@gmail.com>
2025-07-15 17:45:16 +0300

    Update מערכת_המלצות_לסרטים.py

--- מערכת_המלצות_לסרטים.py
+++ מערכת_המלצות_לסרטים.py
@@ -24,7 +24,6 @@ def get_movies():
     while not movie == 'end':
         movies.append(movie)
         movie = input('Enter a movie title (to finish, enter "end"): ')
-    print(movies)
     return movies
 
 def get_ratings(movies):
@@ -81,30 +80,90 @@ def recommend_movies(movie_genres, fav_genres):
                     recommended.update({movie: matching_movies[movie]})
     return recommended
 
+def find_similar_users(target_user, all_users):
+    """
+    This function finds similar users based on the user's favorite movie genres.
+    :param target_user: user to find similar users for
+    :param all_users: dictionary of all users
+    :return: list of similar users
+    """
+    similarities = []
+    for user in all_users.values():
+        if not user.username == target_user.username:
+            common_movies = set(target_user.movies.keys()) & set(user.movies.keys())
+            if common_movies:
+                total_diff = 0
+                for movie in common_movies:
+                    rating1 = target_user.movies[movie][0]
+                    rating2 = user.movies[movie][1]
+                    total_diff += abs(rating1 - rating2)
+                avg_diff = total_diff / len(common_movies)
+                similarities.append((avg_diff, user))
+    similarities.sort(key=lambda x: x[0])
+    return [user for x, user in similarities]
+
+def collaborative_recommendations(target_user, all_users):
+    """
+    This function finds recommendations from similar users.
+    :param target_user: user to find similar movies for
+    :param all_users: dictionary of all users
+    :return: dictionary of recommendations
+    """
+    similar_users = find_similar_users(target_user, all_users)
+    recommended = {}
+    for similar_user in similar_users:
+        for movie, (rating, genre) in similar_user.movies.items():
+            if movie not in target_user.movies and rating >= 8:
+                recommended.update({movie: (rating, genre)})
+        if recommended:
+            break
+    return recommended
+
+# Global users dictionary
+users = {}
+
 def main():
     username = input('Enter username: ')
-    user = User(username)
-    # Print list of movies:
-    user.add_movies(get_genres(get_ratings(get_movies())))
-    print(user.movies)
+    if username not in users:
+        users.update({username: User(username)})
+    user = users[username]
 
-    # Add rating to each movie:
-    # movie_ratings = get_ratings(movies)
+    movies = get_movies()
+    print(movies)
+    movie_ratings = get_ratings(movies)
+    movie_data = get_genres(movie_ratings)
+    user.add_movies(movie_data)
 
     # Print average, max, and min ratings:
-    print(f'Average rating: \033[93m{sum([value[0] for value in user.movies.values()]) / len(user.movies)}\033[0m')
-    print(f'Max rating: \033[92m{max([value[0] for value in user.movies.values()])}\033[0m')
-    print(f'Min rating: \033[91m{min(value[0] for value in user.movies.values())}\033[0m')
-
-    # Add genre to each movie:
-    # movie_genres = get_genres(movie_ratings)
+    ratings = [value[0] for value in user.movies.values()]
+    print(f'Average rating: \033[93m{sum(ratings) / len(ratings)}\033[0m')
+    print(f'Max rating: \033[92m{max(ratings)}\033[0m')
+    print(f'Min rating: \033[91m{min(ratings)}\033[0m')
 
     # Get list of favorite genres from user:
     user.add_fav_genres(get_user_favs())
 
     # Recommend movies based on favorite genres from user and ratings:
     recommended_movies = recommend_movies(user.movies, user.fav_genres)
-    print(f'Recommended movies: {recommended_movies}')
+    print('Recommendations based on favorite genres:')
+    if recommended_movies:
+        for movie, (rating, genre) in recommended_movies.items():
+            print(f'{movie} (Rating: {rating}, Genre: {genre})')
+    else:
+        print('No recommendations.')
+
+    smart_recommendations = collaborative_recommendations(user, users)
+    print('Recommendations from similar users:')
+    if smart_recommendations:
+        for movie, (rating, genre) in smart_recommendations.items():
+            print(f'{movie} (Rating: {rating}, Genre: {genre})')
+    else:
+        print('No recommendations.')
 
 if __name__ == '__main__':
-    main()
+    check = 1
+    while check:
+        main()
+        again = input('Would you like to continue? (y/n): ')
+        if again.lower() == 'n':
+            check = None
