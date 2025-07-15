8e3c706c97c0a4a07548ce4f1710859f186d9523
daniellesalman <salman.danielle@gmail.com>
2025-07-15 15:01:53 +0300

    Update מערכת_המלצות_לסרטים.py

--- מערכת_המלצות_לסרטים.py
+++ מערכת_המלצות_לסרטים.py
@@ -18,7 +18,7 @@ def get_ratings(movies):
     """
     This function gets a rating for each movie in a movie list from the user.
     :param movies: list of movies
-    :return: dictionary of ratings
+    :return: dictionary of movies with ratings
     """
     ratings = {}
     print('Rate each movie:')
@@ -27,15 +27,32 @@ def get_ratings(movies):
         ratings.update({movie: int(rating)})
     return ratings
 
+def get_genres(ratings):
+    """
+    This function gets a genre for each movie in a movie list from the user.
+    :param ratings: dictionary of movies with ratings
+    :return: dictionary of movies with ratings and genres
+    """
+    for movie in ratings:
+        genre = input(f'Enter a genre for "{movie}": ')
+        ratings.update({movie: (ratings[movie], genre)})
+    return ratings
+
 def main():
+    # Print list of movies:
     movies = get_movies()
     print(movies)
+
+    # Add rating to each movie:
     movie_ratings = get_ratings(movies)
 
     # Print average, max, and min ratings:
     print(f'Average rating: \033[93m{sum(movie_ratings.values()) / len(movie_ratings)}\033[0m')
     print(f'Max rating: \033[92m{max(movie_ratings.values())}\033[0m')
     print(f'Min rating: \033[91m{min(movie_ratings.values())}\033[0m')
+
+    # Add genre to each movie:
+    movie_genres = get_genres(movie_ratings)
 
 if __name__ == '__main__':
     main()
